19ba70bd616c2c4b3d4d5c337ff9b0bf4d2eb5cc
Félix Perron-Brault <fpbrault@gmail.com>
2021-11-18 16:30:35 -0500

    feat: 🎸 generate stellar.toml dynamically

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,15 @@
-import { Controller, Get, UseGuards, Request } from '@nestjs/common';
+import { Controller, Get, UseGuards, Request, Header } from '@nestjs/common';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 import { TokenService, DecodedTokenResponse } from './token/token.service';
 import { ApiBearerAuth } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(private readonly tokenService: TokenService) {}
+  constructor(
+    private readonly tokenService: TokenService,
+    private configService: ConfigService,
+  ) {}
 
   /**
    * Decodes and returns the JWT token
@@ -19,5 +23,29 @@ export class AppController {
   @ApiBearerAuth()
   decodeToken(@Request() req: any): Promise<DecodedTokenResponse> {
     return this.tokenService.decodeToken(req.headers.authorization.slice(7));
+  }
+
+  @Get('.well-known/stellar.toml')
+  @Header('content-type', 'text/plain')
+  createToml(): string {
+    return (
+      `ACCOUNTS = [ "` +
+      this.configService.get('source.publicKey') +
+      `" ]
+VERSION = "0.1.0"
+SIGNING_KEY = ""` +
+      this.configService.get('source.publicKey') +
+      `"
+NETWORK_PASSPHRASE = "` +
+      this.configService.get('networkPassphrase') +
+      `"
+WEB_AUTH_ENDPOINT = "` +
+      this.configService.get('homeDomain') +
+      `/auth"
+` +
+      this.configService.get('tomlExtras') +
+      `
+`
+    );
   }
 }
--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -3,10 +3,17 @@ import * as StellarSdk from 'stellar-sdk';
 export default () => ({
   source: {
     keypair: StellarSdk.Keypair.fromSecret(process.env.SERVER_PRIVATE_KEY),
+    publicKey: StellarSdk.Keypair.fromSecret(
+      process.env.SERVER_PRIVATE_KEY,
+    ).publicKey(),
     secretKey: process.env.SERVER_PRIVATE_KEY,
   },
   homeDomain: process.env.HOME_DOMAIN,
-  networkPassphrase: 'Test SDF Network ; September 2015',
+  networkPassphrase:
+    process.env.NETWORK === 'PUBLIC'
+      ? 'Public Global Stellar Network ; September 2015'
+      : 'Test SDF Network ; September 2015',
   jwtSecret: process.env.JWT_SECRET,
   horizonServer: 'https://horizon-testnet.stellar.org',
+  tomlExtras: process.env.TOML_EXTRAS ? process.env.TOML_EXTRAS : '',
 });
